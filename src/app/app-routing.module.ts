458b6317064040075e322a46d0c988cf35387ebc
IronPlus <mav.plus@gmail.com>
2023-10-12 18:14:27 +0300

    Add posts and dashboards component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardsComponent } from './components/dashboards/dashboards.component';
+import { PostsComponent } from './components/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostsComponent },
+  {
+    path: 'posts',
+    component: PostsComponent,
+  },
+  { path: 'dashboards', component: DashboardsComponent },
+  { path: '**', redirectTo: 'posts' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
