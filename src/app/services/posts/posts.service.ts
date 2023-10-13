c4ccb40e8ac77c83225e6f48565c1bd45d9501d4
IronPlus <mav.plus@gmail.com>
2023-10-13 15:16:08 +0300

    Add env

--- src/app/services/posts/posts.service.ts
+++ src/app/services/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Post } from '../../models/post.model';
 
 @Injectable({
@@ -10,6 +11,6 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts(): Observable<Post[]> {
-    return this.http.get<Post[]>('/assets/posts.json'); // todo: move url to a config file
+    return this.http.get<Post[]>(environment.dataUrl);
   }
 }
