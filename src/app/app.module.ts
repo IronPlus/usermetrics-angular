c18c258d8dc90eee1601aa96e04ce2edfb4c9c52
IronPlus <mav.plus@gmail.com>
2023-10-13 11:31:43 +0300

    Add toolbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +18,8 @@ import { PostsComponent } from './components/posts/posts.component';
     HttpClientModule,
     BrowserAnimationsModule,
     MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
