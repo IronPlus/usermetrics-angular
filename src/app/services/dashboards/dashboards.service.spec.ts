6c3e61fed8efa173fe5df3181941f09ddd045a5a
IronPlus <mav.plus@gmail.com>
2023-10-17 15:52:05 +0300

    Fix dashboard service test.

--- src/app/services/dashboards/dashboards.service.spec.ts
+++ src/app/services/dashboards/dashboards.service.spec.ts
@@ -1,16 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DashboardsService } from './dashboards.service';
 
 describe('DashboardsService', () => {
   let service: DashboardsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DashboardsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // TODO: Add tests for the other methods.
 });
